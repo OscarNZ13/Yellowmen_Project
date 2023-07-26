645c77bedc49fd3e2c3a3530744dca4c87590f9d
OscarNZ13 <133930637+OscarNZ13@users.noreply.github.com>
2023-07-26 11:05:55 -0600

    Creando CRUD HTML (1st)

--- src/main/java/main/Controllers/CRUDController.java
+++ src/main/java/main/Controllers/CRUDController.java
@@ -1,19 +1,32 @@
-
 package main.Controllers;
+
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import main.Entities.Clothes;
+import main.service.IClothesService;
 
 @Controller
 @RequestMapping("/test")
 public class CRUDController {
-    public CRUDController(){
-        
+
+    private final IClothesService clothesService;
+
+    public CRUDController(IClothesService clothesService) {
+        this.clothesService = clothesService;
     }
+
     @GetMapping()
-    public String CRUDController(Model model){
+    public String CRUDController(Model model, @RequestParam("lowerPrice") Optional<Integer> lowerPrice, @RequestParam("higherPrice") Optional<Integer> higherPrice) {
+        model.addAttribute("titulo", "Yellow Men");
+        var baseClothes = new Clothes();
+        model.addAttribute("clothesDefault", baseClothes);
+        model.addAttribute("clothes", this.clothesService.getProductsWithFilters(lowerPrice, higherPrice));
         return "test";
     }
 }
